b6acfdfd7ff69739cd511a94f5eab24a7a34d90d
Solomon Kieffer <solomonkeiffer@MacBook-Pro-80.local>
2018-10-15 12:40:22 -0500

    Initial Commit

---
+++ Video Game Library iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Video Game Library iOS
+//
+//  Created by Solomon Kieffer on 10/15/18.
+//  Copyright © 2018 Phoenix Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
